91612e2e4139b4e885345d065d4119970ef7aa7c
SrivastavaCharu <charusri97@gmail.com>
2024-12-04 13:15:11 +0530

    Added models

--- sms_notifications/models.py
+++ sms_notifications/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    phone_number = models.CharField(max_length=15, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.phone_number
+
+
+class FlashSale(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
